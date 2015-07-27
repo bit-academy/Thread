7ba4c4b36b8c435681009aa83eaf2ddcc015e8ef
mindwing <mindwing@gmail.com>
2015-07-28 02:59:22 +0900

    .

--- ThreadTest2.java
+++ ThreadTest2.java
@@ -7,37 +7,21 @@ public class ThreadTest2 extends Thread
 
     public void run()
     {
-        for (int i=0; i<10; i++)
+        for (int i=0; i<1000; i++)
         {
-            System.out.print(getName()+" ");
-            try
-            {
-                    sleep(400);
-            } catch(InterruptedException ie)
-            {
-            }
+            System.out.print(getName());
         }
     }
 
     public static void main(String[] args)
     {
-        ThreadTest2 thread1=new ThreadTest2("스레드1");
-        ThreadTest2 thread2=new ThreadTest2("스레드2");
+        ThreadTest2 thread1=new ThreadTest2("1");
+        ThreadTest2 thread2=new ThreadTest2("2");
 
         thread1.setPriority(MIN_PRIORITY);
         thread2.setPriority(MAX_PRIORITY);
 
         thread1.start();
         thread2.start();
-
-        try
-        {
-                Thread.sleep(2000);
-        } catch(InterruptedException ie)
-        {
-        }
-
-        thread1.setPriority(MAX_PRIORITY);
-        thread2.setPriority(MIN_PRIORITY);
     }
 }
